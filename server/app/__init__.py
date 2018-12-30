87d027d68493eb4a52477d26dce16ba1ddfb0ebb
David Grigoryan <30605480+Dabudi@users.noreply.github.com>
2018-12-31 00:26:53 +0300

    Update __init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from gensim.models.fasttext import FastText
+import fasttext
 import sqlite3 as sql
 DB_PATH = 'C:/Users/User/Desktop/VK_HACK/VK_HACK/server/app/tables.db'
 
-nlpModel = FastText.load('app/nlpModel/araneum_none_fasttextskipgram_300_5_2018.model')
+nlpModel = fasttext.load_model('app/nlpModel/wiki.ru.bin')
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config.from_object(Config)
